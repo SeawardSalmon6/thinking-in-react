69003c81f910e95543ae3c16dbfdc4c7a68fc15d
SeawardSalmon6 <rayhanachour@hotmail.com>
2022-03-22 11:16:59 -0300

    feat: adding fake data and thinking about components hierarchy

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,45 @@
+const date = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball",
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball",
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch",
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5",
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" },
+];
+
+/* ======== App Hierarchy Design
+  --> FilterableProductTable
+      --> SearchBar
+      --> ProductTable
+          -> ProductCategoryRow
+          -> ProductRow
+*/
+
 function App() {
   return;
 }
